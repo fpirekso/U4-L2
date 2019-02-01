3b2dfa80c17e0fe9f32bed4ac4b1ba1df3469dbb
fpirekso <43756366+fpirekso@users.noreply.github.com>
2019-02-01 10:12:02 -0500

    Add files via upload

---
+++ Lesson 2/problem1.py
@@ -0,0 +1,3 @@
+classes = ['english','math','history','science','gym','health','advisory','production']
+for classes in classes:
+	print('You have been enrolled in ' + classes + '.')
---
+++ Lesson 2/problem2.py
@@ -0,0 +1,4 @@
+numbers = [3,5,9,17,123,666,1000,4321]
+
+for numbers in numbers:
+	print(numbers * 2)
